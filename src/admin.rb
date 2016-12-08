8d794401b8441c5b3058ebe6e9b4335bc8630892
atri-konami <freude1994@gmail.com>
2016-12-08 16:52:44 +0900

    fix typo

--- src/admin.rb
+++ src/admin.rb
@@ -13,7 +13,7 @@
     before do
         @client = Twitter::REST::Client.new do |config|
             config.consumer_key = ENV['CONSUMER_KEY']
-            config.consumer_secret = ENV['CONSUMER_SERCRET']
+            config.consumer_secret = ENV['CONSUMER_SECRET']
             config.access_token = ENV['ACCESS_TOKEN']
             config.access_token_secret = ENV['ACCESS_TOKEN_SECRET']
         end
