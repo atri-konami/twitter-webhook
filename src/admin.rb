d74760e009e6330cefca9ba742f00349f8be6026
atri-konami <freude1994@gmail.com>
2016-12-08 16:44:21 +0900

    change access token secret key

--- src/admin.rb
+++ src/admin.rb
@@ -13,7 +13,7 @@
             config.consumer_key = ENV['CONSUMER_KEY']
             config.consumer_secret = ENV['CONSUMER_SERCRET']
             config.access_token = ENV['ACCESS_TOKEN']
-            config.access_token_secret = ENV['ACCESS_SECRET']
+            config.access_token_secret = ENV['ACCESS_TOKEN_SECRET']
         end
     end
 
