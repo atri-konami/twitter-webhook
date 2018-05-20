2f055c9d02b11cccd4bf1245d8613b2d3bc3000e
atri konami <freude1994@gmail.com>
2018-05-20 22:41:55 +0900

    bugfix

--- unicorn.rb
+++ unicorn.rb
@@ -1,13 +1,12 @@
 @path = ""
 
-working_processes 1
+worker_processes 1
 working_directory @path
 preload_app true
 
 timeout 300
-listen 80
+listen "/tmp/api.sock"
 
 pid "#{@path}tmp/pids/unicorn.pid"
-
 stderr_path "#{@path}log/unicorn.stderr.log"
 stdout_path "#{@path}log/unicorn.stdout.log"
